1e7bee7aa100ad454cf2c035cf8870234d6a7151
Evan Schwander <eschwander@csumb.edu>
2015-09-17 11:55:48 -0700

    Finished

--- picCombiner.py
+++ picCombiner.py
@@ -1,21 +1,56 @@
 import os
+import time
+import types
 
-def mySort(myList):
-  for i in range(0,len(myList) - 1):
-    for j in range(i+1, len(myList)):
-      if myList[j] < myList[i]:
-        temp = myList[i]
-        myList[i] = myList[j]
-        myList[j] = temp
-        print(1)
+def myMergeSort(myList):
+  length = len(myList)
+  if length == 1:
+    return myList[0]
+  x = myList[0:length // 2]
+  y = myList[length // 2 : length]
+  return myMerge(myMergeSort(x),myMergeSort(y))
+  
+def myMerge(xList, yList):
+  x = []
+  y = []
+  if isinstance(xList, list):
+    x.extend(xList)
+  else:
+    x.append(xList)
+  if isinstance(yList, list):
+    y.extend(yList)
+  else:
+    y.append(yList)
+  mergeList = []
+  xCount = 0
+  yCount = 0
+  end = len(x) + len(y)
+  for _ in range(0,end):
+    if xCount < len(x):
+      if yCount < len(y):
+        if y[yCount] < x[xCount]:
+          mergeList.append(y[yCount])
+          yCount+=1
+        else:
+          mergeList.append(x[xCount])
+          xCount+=1
+      else:
+        mergeList.extend(x[xCount:])
+        break
+    else:
+      mergeList.extend(y[yCount:])
+      break
+  return mergeList
 
-
-pics = []
-pix = []
-
+  
+  
+startTime = time.time() # this is used to measure run time 
+pics = [] # holds all the pictures in a folder
+pix = [] # Holds pixels for all pictures in pix
 
 path = "C:\\cygwin64\\home\\CSUMB\\CST205\\Project1\\pictures\\"
 
+#the following loop adds all pictures in given path into the pics list
 flag = True
 for file in os.listdir(path):
   if file.endswith('.png'):
@@ -25,9 +60,12 @@ for file in os.listdir(path):
       finalPic = makePicture(path + file)
       finalPix = getPixels(finalPic)
 
+#the following loop adds all pixles of the pictures in the pix list into pix
 for pic in pics:
   pix.append(getPixels(pic))
  
+#This loop is the meat of the program. It finds the rgb color values
+# across all pixels in a single location and finds the meadian of each
 for i in range(0,len(finalPix)):
   redPix = []
   greenPix = []
@@ -37,15 +75,19 @@ for i in range(0,len(finalPix)):
     greenPix.append(getGreen(pix[j][i]))  
     bluePix.append(getBlue(pix[j][i])) 
   redPix.sort()
+  #redPix = myMergeSort(redPix)
   redPixel = redPix[len(redPix)//2]
   greenPix.sort()
+  #greenPix = myMergeSort(greenPix)
   greenPixel = greenPix[len(greenPix)//2]
   bluePix.sort()
+  #bluePix = myMergeSort(bluePix)
   bluePixel = bluePix[len(bluePix)//2]
   finalPix[i].setRed(redPixel)
   finalPix[i].setGreen(greenPixel)
   finalPix[i].setBlue(bluePixel)
     
+#show the final pic and save it
 show(finalPic)
 writePictureTo(finalPic, path + "/final/final.png")
-  
+print(time.time() - startTime)
